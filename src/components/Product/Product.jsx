8afb5d425d1ecc9c4f58f24a7f3b8d8116052d6b
Tasin Ahammed <freelancertasin507@gmail.com>
2023-03-28 03:01:43 +0600

    added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import './Product.css'
 
 const Product = (props) => {
     const {img,name,seller,price,ratings} = props.product
+    const handleproduct = props.handleproduct
+   
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -12,7 +16,10 @@ const Product = (props) => {
             <small>Manufacturer: {seller}</small> <br />
             <small>Ratings: {ratings} Stars</small>
             </div>
-            <button className='button-cart'>Add to Cart</button>
+            <button onClick={() => handleproduct(props.product)} className='button-cart'>
+                Add to Cart
+                <FontAwesomeIcon icon={faShoppingCart} />
+                </button>
         </div>
     );
 };
