b4af6bab409ad70050817e2440eb0b748567fc69
Tasin Ahammed <freelancertasin507@gmail.com>
2023-03-29 00:09:39 +0600

    added

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
 import "./Shop.css";
 
@@ -25,8 +26,8 @@ const Shop = () => {
           ></Product>
         ))}
       </div>
-      <div className="cart-container">
-        <h3>Order Summary </h3>
+      <div>
+        <Cart cart={cart}></Cart>
       </div>
     </div>
   );
