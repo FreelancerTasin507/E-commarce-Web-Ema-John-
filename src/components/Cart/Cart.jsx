11c90f7795177be883b1323a0e920b62f3e13909
Tasin Ahammed <freelancertasin507@gmail.com>
2023-03-30 00:58:18 +0600

    show data from api

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,10 +7,14 @@ const Cart = (props) => {
     console.log(cart);
     let totalPrice = 0 ;
     let totalShipping = 0;
-
+    let quantity = 0;
+    
     for (const product of cart){
-        totalPrice = totalPrice + product.price
-        totalShipping = totalShipping + product.shipping
+        product.quantity = product.quantity || 1
+        totalPrice = totalPrice + product.price * product.quantity;
+        totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
+        
     }
     const tax = totalPrice*7/100;
     const grandTotal = totalPrice+totalShipping+tax;
@@ -18,7 +22,7 @@ const Cart = (props) => {
     return (
         <div className='cart-container'>
             <h3>Order Summary </h3>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: ${totalPrice}</p>
             <p>Total Shipping: ${totalShipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
