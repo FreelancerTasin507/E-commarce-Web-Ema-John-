fa291628529fd64eb712a305356c8471dead38ce
Tasin Ahammed <freelancertasin507@gmail.com>
2023-04-05 00:55:05 +0600

    cart share to another route

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Cart from '../Cart/Cart';
 
 const Orders = () => {
+    const cart =  useLoaderData()
+    console.log(cart);
     return (
-        <div>
-            <h1>Orders Page</h1>
+        <div className='shop-container'>
+            <div className='product-container'>
+            <h2>Orders Page: {cart.length}</h2>
+            </div>
+            <div>
+                <Cart  cart={cart} ></Cart>
+            </div>
         </div>
     );
 };
